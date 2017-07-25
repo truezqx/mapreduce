767e56fa0bfd898a8d7546057bddb6409c236428
zqx <397524786@qq.com>
2017-07-25 19:11:37 +0800

    Signed-off-by: zqx <397524786@qq.com>

---
+++ src/cn/zqx/profit/ProfitDriver.java
@@ -0,0 +1,28 @@
+package cn.zqx.profit;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ProfitDriver {
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(ProfitMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setReducerClass(ProfitReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.setInputPaths(job, new Path("hdfs://192.168.21.128:9000/profit"));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://192.168.21.128:9000/profit/result"));
+		job.waitForCompletion(true);
+	}
+
+}
---
+++ src/cn/zqx/profit/ProfitMapper.java
@@ -0,0 +1,19 @@
+package cn.zqx.profit;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class ProfitMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		String line = value.toString();
+		String month = line.split(" ")[0];
+		int profit = Integer.parseInt(line.split(" ")[1]);
+		context.write(new Text(month), new IntWritable(profit));
+	}
+}
