9c048042157bbf6c6493bc046664e95f60639600
zqx <397524786@qq.com>
2017-07-26 14:01:06 +0800

    Signed-off-by: zqx <397524786@qq.com>

---
+++ src/cn/zqx/zebra/ZebraDriver.java
@@ -0,0 +1,28 @@
+package cn.zqx.zebra;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ZebraDriver {
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(ZebraMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(HttpAppHost.class);
+		job.setReducerClass(ZebraReducer.class);
+		job.setOutputKeyClass(HttpAppHost.class);
+		job.setOutputValueClass(NullWritable.class);
+		FileInputFormat.setInputPaths(job, new Path("hdfs://192.168.21.128:9000/zebra"));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://192.168.21.128:9000/zebra/result"));
+		job.waitForCompletion(true);
+
+	}
+
+}
